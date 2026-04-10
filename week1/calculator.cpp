9d81c0e20bc89e6e5c351d6f00933781b70506ae
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-04-09 17:04:30 -0700

    Update calculator.cpp

--- week1/calculator.cpp
+++ week1/calculator.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 int main() {
     double num1, num2;
-    std:: 
+    std::cout << "Enter 2 numbers: ";
     std::cin >> num1 >> num2;
+    std::sum = num1 + num2;
+    std::subtract = num1-num2;
+    std::multiply = num1*num2;
+    std::divide = num1/num2;
+    std::cout << "Results:"
     return 0;
 }
