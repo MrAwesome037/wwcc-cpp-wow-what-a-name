64bdebe3b84b6a438c12697e39f591fd654300c4
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-04-11 23:17:01 -0700

    Update temp_converter.cpp

--- week1/temp_converter.cpp
+++ week1/temp_converter.cpp
@@ -2,6 +2,9 @@
 int main() {
     double temperature;
     char unit;
-    
+    std::cout << "Enter temperature value: "; 
+    std::cin >> temperature;
+    std::cout << "Enter unit (C for Celsius, F for Fahrenheit): "; 
+    std::cin >> unit;
     return 0;
 }
