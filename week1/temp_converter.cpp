de12f3c613893a749d16ccf3ae1ca1ff4615f7e9
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-04-11 23:25:47 -0700

    Update temp_converter.cpp

--- week1/temp_converter.cpp
+++ week1/temp_converter.cpp
@@ -7,9 +7,11 @@ int main() {
     std::cout << "Enter unit (C for Celsius, F for Fahrenheit): "; 
     std::cin >> unit;
     if(unit == "F"){
-        std::cout << "
+        std::string sentence = std::to_string(temperature) + "°F is equal to " + std::to_string((temperature - 32) * 5/9) + "°C";
+        std::cout << sentence;
     } else{
-        
+        std::string sentence = std::to_string(temperature) + "°C is equal to " + std::to_string((temperature * 9/5) + 32) + "°F";
+        std::cout << sentence;
     }
     return 0;
 }
