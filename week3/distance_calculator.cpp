bd23378d4c0342e1f4ec163cf19ec429bcd5c529
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-06 22:35:14 -0700

    Update distance_calculator.cpp

--- week3/distance_calculator.cpp
+++ week3/distance_calculator.cpp
@@ -1,6 +1,10 @@
-#include <iosstream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 double calculateDistance(double x1, double y1, double x2, double, y2){
   return sqrt((x₂ - x₁)**2 + (y₂ - y₁)**2);
+}
+int main() {
+    
+    return 0;
 }
