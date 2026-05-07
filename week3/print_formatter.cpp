aa00bb3269ce2a3c77e3503a8bc8faa99f8135d7
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-07 05:47:06 +0000

    Create print_formatter.cpp

---
+++ week3/print_formatter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Overloaded functions
+int print() {
+    
+}
+
+double print() {
+    
+}
+
+// Different number of parameters
+int print() {
+    
+}
+
+int main() {
+    
+}
