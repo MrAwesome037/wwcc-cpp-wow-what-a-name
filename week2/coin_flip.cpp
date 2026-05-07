0ac54e379ff292e3d376aa85f2d2e0b56c150d47
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-06 22:05:23 -0700

    Create coin_flip.cpp

---
+++ week2/coin_flip.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>  // For rand()
+#include <ctime>    // For time()
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int flips;
+    int heads = 0;
+    int tails = 0;
+
+    cout << "Coin Flip Simulator" << endl;
+    cout << "How many flips would you like to simulate? ";
+    cin >> flips;
+    for (int i = 1; i <= flips; i++) {
+        int randomNum = rand() % 3;
+        if(randomNum == 1){
+          heads += 1;
+        }
+        else{
+          tails += 1;
+        }
+    }
+    printf("Heads: %d", heads);
+    printf("Tails: %d", tails);
+    return 0;
+}
