1de70125a0755b54f6e4cb077fd17b2f520416f2
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-07 05:51:45 +0000

    Update distance_calculator.cpp

--- week3/distance_calculator.cpp
+++ week3/distance_calculator.cpp
@@ -10,11 +10,11 @@ int main() {
     double x2;
     double y2;
     cout << "Enter the First Point: ";
-    cin >> x1;
-    cin >> y1;
+    cin >> "x1:" >> x1;
+    cin >> "y1:" >> y1;
     cout << "Enter the Second Point: ";
-    cin >> x2;
-    cin >> y2;
+    cin >> "x2:" >> x2;
+    cin >> "y2:" >> y2;
     printf("The distance between (%f, %f) and (%f, %f) is %f", x1, y1, x2, y2, calculateDistance(x1,y1,x2,y2));
     return 0;
 }
