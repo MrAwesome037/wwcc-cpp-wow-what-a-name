97363b9961f6e1ddc72f6831c984ddaf54165f91
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-07 21:15:53 -0700

    Create currency_converter.cpp

---
+++ week3/currency_converter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+double dollarToEuro(double amount){
+}
+double dollarToEuro(double amount){
+}
+double dollarToEuro(double amount){
+}
+int getConversionCount(){
+  return conversions;
+}
+int main() {
+    int conversions = 0;
+    return 0;
+}
