a2eea22a95de4e82a340753cf4da260c85174175
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-07 20:52:54 -0700

    Update temperature_converter.cpp

--- week3/temperature_converter.cpp
+++ week3/temperature_converter.cpp
@@ -3,15 +3,15 @@
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #endif
-
+void convertTemperature(double temp, char unit){
+  std::cout << "The temperature is 72\xC2\xB0F" << std::endl;
+}
 int main() {
     // Platform-specific setup for Windows
     #if defined(_WIN32) || defined(_WIN64)
     SetConsoleOutputCP(CP_UTF8);
     #endif
-
-    // Standard UTF-8 degree symbol sequence
-    std::cout << "The temperature is 72\xC2\xB0F" << std::endl;
+    
 
     return 0;
 }
