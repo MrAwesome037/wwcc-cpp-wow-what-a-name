1a7be749da1fa061efb585b1c98f8e37a7c80a27
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-09 23:10:56 +0000

    Update text_analyzer.cpp

--- week4/text_analyzer.cpp
+++ week4/text_analyzer.cpp
@@ -1,1 +1,30 @@
-
+#include <iostream>
+#include <sstream>
+using namespace std;
+int main() {
+    string sentence;
+    int sentences = 0;
+    print("Enter a sentence or paragraph: ")
+    getline(cin, sentence);
+    int characters = sentence.length();
+    string stripped = sentence;
+    stringstream ss(sentence);
+    string longestword = ss[0];
+    words = ss.length();
+    for(i = 0; i <= characters; i++){
+      if(sentence[i] == "." or sentence[i] == "!" or sentence[i] == "?"){
+        sentences += 1;
+      }
+    }
+    for(i = 0, 
+    if(ss[i].length() > longestword.length()){
+        longestword = ss[i];
+      }
+    print("Analysis: ");
+    printf("Characters (with spaces): %d", characters);
+    printf("Word count: %d", words);
+    printf("Sentence count: %d," sentences);
+    print("Longest word: " + longestword);
+    printf("Average word length: %f characters", stripped.length()/words);
+    return 0;
+}
