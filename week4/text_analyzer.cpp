b7ab80df8cae12a456b58b2dc7e5515133076755
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-09 23:21:38 +0000

    Update text_analyzer.cpp

--- week4/text_analyzer.cpp
+++ week4/text_analyzer.cpp
@@ -3,28 +3,39 @@
 using namespace std;
 int main() {
     string sentence;
+    string upper;
+    string lower;
     int sentences = 0;
     print("Enter a sentence or paragraph: ")
     getline(cin, sentence);
     int characters = sentence.length();
     string stripped = sentence;
-    stringstream ss(sentence);
-    string longestword = ss[0];
-    words = ss.length();
     for(i = 0; i <= characters; i++){
       if(sentence[i] == "." or sentence[i] == "!" or sentence[i] == "?"){
         sentences += 1;
       }
+        upper += toupper(sentence[i]);
+        lower += tolower(sentence[i]);
     }
-    for(i = 0, 
-    if(ss[i].length() > longestword.length()){
-        longestword = ss[i];
-      }
+    for(i = 0; i <= stripped.length(); i++){
+        if(ispunct(stripped[i])){
+            stripped.erase(i--, 1);
+        }
+    }
+    stringstream ss(stripped);
+    words = ss.length();
+    string longestword = ss[0];
+    for(i = 0; i <= words; i++){
+        if(ss[i].length() > longestword.length()){
+            longestword = ss[i];
+        }
     print("Analysis: ");
     printf("Characters (with spaces): %d", characters);
     printf("Word count: %d", words);
     printf("Sentence count: %d," sentences);
     print("Longest word: " + longestword);
     printf("Average word length: %f characters", stripped.length()/words);
+    print("Uppercase: " + upper);
+    print("Lowercase: " + lower);
     return 0;
 }
