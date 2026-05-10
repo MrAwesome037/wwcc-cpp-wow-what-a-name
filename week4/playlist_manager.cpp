afbaa669d52ecd867af6049bc0389e956e6a1230
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-09 17:22:25 -0700

    Create playlist_manager.cpp

---
+++ week4/playlist_manager.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Song {
+    struct Artist{
+      string name;
+      string hometown;
+    };
+    int durationSeconds;
+    string genre;
+    string title;
+};
+int main() {
+    Song playlist[5];
+    for(i = 1; i <= 5; i++){
+      
+    return 0;
+}
