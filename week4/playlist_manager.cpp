18c391147398e1e2784fe8d5174f64dd5b76f4cc
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-05-10 13:39:06 -0700

    Update playlist_manager.cpp

--- week4/playlist_manager.cpp
+++ week4/playlist_manager.cpp
@@ -19,6 +19,8 @@ int main() {
     string temp_artist;
     string genre_search;
     string temp_genre;
+    bool found = false;
+    Song search_list[5];
     for(i = 1; i <= 5; i++){
         printf("Enter information for song %f: ", i);
         print("Title: ");
@@ -50,6 +52,46 @@ int main() {
     printf("Longest song: %s (%d:%d)", longest.title, longest.duration / 60, longest.duration %60);
     print("Search by artist: ");
     cin >> artist_search;
+    printf("Songs by %s", artist_search);
     transform(artist_search.begin(), artist_search.end(), artist_search.begin(), [](unsigned char c){ return tolower(c);});
+    for(i = 0; i < 4; i++){
+       temp_artist = playlist[i].name;
+         transform(temp_artist.begin(), temp_artist.end(), temp_artist.begin(), [](unsigned char c){ return tolower(c);});
+        if(temp_artist == artist_search){
+            found = true;
+            search_list[i] = playlist[i];
+        }
+    }
+    
+    if(found == false){
+        print("No Results");
+    }else{
+        for(i = 0; i < 4; i++){
+            if(search_list[i] != ""){
+                printf("%s (%d:%d)", search_list[i].title, search_list[i].duration / 60, search_list[i].duration %60);
+        }
+    }
+    print("Search by genre: ");
+    cin >> genre_search;
+    printf("Songs by %s", genre_search);
+    found = false;
+    search_list[5] = {};
+    transform(genre_search.begin(), genre_search.end(), genre_search.begin(), [](unsigned char c){ return tolower(c);});
+    for(i = 0; i < 4; i++){
+       temp_artist = playlist[i].name;
+         transform(temp_genre.begin(), temp_genre.end(), temp_genre.begin(), [](unsigned char c){ return tolower(c);});
+        if(temp_genre == genre_search){
+            found = true;
+            search_list[i] = playlist[i];
+        }
+    }
+    if(found == false){
+        print("No Results");
+    }else{
+        for(i = 0; i < 4; i++){
+            if(search_list[i] != ""){
+                printf("%s (%d:%d)", search_list[i].title, search_list[i].duration / 60, search_list[i].duration %60);
+        }
+    }
     return 0;
 }
