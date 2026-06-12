e25a385b159608f4f30bdd4ac88666240809e139
Kieran Herrmann <herrmann.kieran@gmail.com>
2026-06-11 22:46:58 -0700

    Update story_game.cpp

--- FINAL_PROJECT/story_game.cpp
+++ FINAL_PROJECT/story_game.cpp
@@ -1,1 +1,37 @@
-
+/*
+ * Interactive Story Game - Totally Legit Spy Game
+ * Author: Kieran K. Herrmann
+ * Date: [Submission Date]
+ *
+ * ==== Project Features ====
+ * Variables (3+ types): Lines __
+ * If/else: Lines __
+ * Switch: Line __
+ * Loop: Lines __
+ * Function w/ return value: Line __
+ * Void function: Line __
+ * Function w/ parameters: Line __
+ * Vector or array: Lines __
+ * Struct or class: Lines __
+ * Enum: Lines __
+ * File reading: Lines __
+ * File writing: Lines __
+ * Modern feature 1 (____): Lines __
+ * Modern feature 2 (____): Lines __
+ * ==========================
+ */
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+class Player{
+  private:
+    int health = 10;
+  public:
+    Player(string n){
+      string name = n;
+    }
+};
+int main() {
+    return 0;
+}
